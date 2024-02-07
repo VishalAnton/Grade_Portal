de22b43c283142fbe4b1436f4b407330aeb06d66
VishalAnton <vishalanton2004@gmail.com>
2024-02-07 23:47:53 +0530

    Adding PostgreSQL

---
+++ src/main/java/com/ltp/gradesubmission/connectpostgresqldb/GradeEntity.java
@@ -0,0 +1,72 @@
+package com.ltp.gradesubmission.connectpostgresqldb;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "grades")
+public class GradeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "score")
+    private String score;
+
+    public GradeEntity() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public GradeEntity(String name, String subject, String score) {
+        this();
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
